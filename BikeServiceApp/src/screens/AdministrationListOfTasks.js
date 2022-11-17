cddc7903c3bb5d303329a0766ad8c9b93233e5a1
Maniek13 <mani3k1989@gmail.com>
2022-11-17 04:39:37 +0100

    back behavior from administartion

--- BikeServiceApp/src/screens/AdministrationListOfTasks.js
+++ BikeServiceApp/src/screens/AdministrationListOfTasks.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, BackHandler} from 'react-native';
 
 class AdministrationListOfTasks extends Component {
+  constructor(props){
+    super(props);
+    this.handleBackButton = this.handleBackButton.bind(this);
+  }
+  componentDidMount (){
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  handleBackButton(){
+    this.props.navigate('Main');
+    return true;
+  } 
+  
   render() {
     return (
       <View>
